055fb0b9c023c2030d99e89b0aec0004eeddcbb8
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-31 23:25:02 +0300

    then, catch, finally

--- trial.js
+++
@@ -1,9 +0,0 @@
-// ------- Prpmise  -----------
-
-let check = true
-const promise1 = new Promise((resolve, reject)=>{
-    if (check) {resolve("Promise is succesfull") }
-    else { reject("Promise is unseccesfull")}
-})
-
-console.log(promise1)
