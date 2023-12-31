5b1614ba7ae93e24be1a4fc5d7948807f34864fd
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-31 23:09:28 +0300

    Promise created

--- javascript.js
+++ javascript.js
@@ -14,3 +14,57 @@
 // setTimeout(() => console.log("0.5 sec wait and run"), 500)
 
 // console.log("Kerem")
+
+// callBack : 
+// As passing parameters to another function 
+// make asyncronous structure to syncronous
+
+// getName = (callback) => {
+//     setTimeout(()=> {
+//         console.log("Hakan");
+//         callback()
+//     },1000)   
+// }
+// getSurname = () => {
+//     setTimeout(()=>console.log("Pekkaya"),500)
+// }
+
+// getName(getSurname)
+
+// // 2nd alternative
+
+// getName1 = (callback) => {
+//     setTimeout(()=> {
+//         let name = "Hakan";
+//         callback(name)
+//     },1000)   
+// }
+// getSurname1 = (name) => {
+//     setTimeout(()=> {
+//         let surname = "Pekkaya";
+//         console.log(name, surname);
+//     },500)       
+// }
+// getName1(getSurname1)
+
+
+// // 3rd alternative
+
+// getName2 = (callback) => {
+//     setTimeout(()=> {
+//         let name = "Hakan";
+//         callback(name)
+//     },1000)   
+// }
+// getSurname2 = (name, surname) => {
+//     setTimeout(()=> {
+//         let surname = "Pekkaya";
+//         callback(surname);
+//     },500)       
+// }
+// getName2((name)=>{
+//     getSurname2(name, (surname)=>{
+//         console.log(name, surname)
+//     })
+// })
+
