a8076fbd21e58445e6ac049b2fb1a81a6905b5ce
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-31 23:44:12 +0300

    try catch

---
+++ trial.js
@@ -0,0 +1,23 @@
+// ---- promise - XMLHttpRequest
+
+
+function readStudents(url) {
+    return new Promise((resolve, reject)=>{
+        const xhr = new XMLHttpRequest();
+        try {
+            xhr.addEventListener("readystatechange", ()=>{
+                if(xhr.readyState===4 && xhr.status===200) {
+                    resolve(JSON.parse(xhr.responseText))
+                }
+            })
+        } catch (error) {
+            reject(error)
+        }
+        xhr.open("GET",url);
+        xhr.send();
+    })
+}
+
+readStudents("students.json")
+.then((data)=>console.log(data))
+.catch((err)=>console.log(err))
