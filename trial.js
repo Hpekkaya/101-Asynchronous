9b259c17ef0807ce8e1721252f05a523b65cfd9d
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-31 23:58:43 +0300

    get from JSON

--- trial.js
+++ trial.js
@@ -11,6 +11,7 @@ function readStudents(url) {
                 }
             })
         } catch (error) {
+            console.log("Problem in JSON !")
             reject(error)
         }
         xhr.open("GET",url);
@@ -18,6 +19,31 @@ function readStudents(url) {
     })
 }
 
-readStudents("students.json")
+// readStudents("students.json")
+// .then((data)=>console.log(data))
+// .catch((err)=>console.log("Error !",err))
+
+function getUsers(url){
+    const xhr = new XMLHttpRequest()
+    xhr.addEventListener("readystatechange", ()=> {
+        try {
+            if (xhr.readyState===4 && xhr.status===200){
+                console.log(JSON.parse(xhr.responseText))
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+    xhr.open("GET", url);
+    xhr.send();
+}
+getUsers("https://jsonplaceholder.typicode.com/users")
 .then((data)=>console.log(data))
 .catch((err)=>console.log("Error !",err))
+.finaly(()=> ) //Write mail codes)
+
+
+
+// readStudents("students.json")
+// .then((data)=>console.log(data))
+// .catch((err)=>console.log("Error !",err))
