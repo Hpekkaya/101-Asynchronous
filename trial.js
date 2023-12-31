00f1921f4dab2b3a82818ab263418a8a76b6abdb
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-31 16:22:32 +0300

    setTimeout

---
+++ trial.js
@@ -0,0 +1,17 @@
+// JavaScript runs as Syncronous Programing Language
+
+// JavaScript as Asyncromous 
+// 1-Timings
+// 2-Events
+// 3-HTTP Requests
+
+// WEB API's
+
+console.log("Hakan")
+
+setTimeout(() => console.log("1 sec wait and run"), 1000)
+setTimeout(() => console.log("0.5 sec wait and run"), 500)
+
+
+console.log("Kerem")
+
