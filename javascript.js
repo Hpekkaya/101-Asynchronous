de5252a3ae83315323d371f930e5f95724d86379
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-31 18:45:55 +0300

    2 alternative callback tried

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// JavaScript runs as Syncronous Programing Language
+
+// JavaScript as Asyncronous 
+// 1-Timings
+// 2-Events
+// 3-HTTP Requests
+// WEB API's
+
+// Callback - Promise - async & await 
+
+// console.log("Hakan")
+
+// setTimeout(() => console.log("1 sec wait and run"), 1000)
+// setTimeout(() => console.log("0.5 sec wait and run"), 500)
+
+// console.log("Kerem")
--- trial.js
+++ trial.js
@@ -17,7 +17,6 @@ getName(getSurname)
 // 2nd alternative
 
 getName1 = (callback) => {
-    debugger;
     setTimeout(()=> {
         let name = "Hakan";
         callback(name)
@@ -29,8 +28,26 @@ getSurname1 = (name) => {
         console.log(name, surname);
     },500)       
 }
-
-debugger;
 getName1(getSurname1)
 
+
+// 3rd alternative
+
+getName2 = (callback) => {
+    setTimeout(()=> {
+        let name = "Hakan";
+        callback(name)
+    },1000)   
+}
+getSurname2 = (name, surname) => {
+    setTimeout(()=> {
+        let surname = "Pekkaya";
+        callback(surname);
+    },500)       
+}
+getName2((name)=>{
+    getSurname2(name, (surname)=>{
+        console.log(name, surname)
+    })
+})
 
