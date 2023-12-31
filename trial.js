22be00e544b5f82392b381dea4d2a1d91706f878
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-31 22:14:24 +0300

    AJAX XMLHttpRequest

--- trial.js
+++ trial.js
@@ -1,53 +1,31 @@
-// callBack : Bir fonksiyou bir fonksiyona parametre geçerek
-// asenkron yapıyı senkrona çevirir
-// As assing parameters to another function
+// AJAX
 
-getName = (callback) => {
-    setTimeout(()=> {
-        console.log("Hakan");
-        callback()
-    },1000)   
-}
-getSurname = () => {
-    setTimeout(()=>console.log("Pekkaya"),500)
+// const xhr = new XMLHttpRequest();
+// console.log(xhr);
+
+function prepareURL(url, id) {
+    if(id===null) {
+        return url;
+    }
+    return `${url}?postId=${id}`
 }
 
-getName(getSurname)
-
-// 2nd alternative
-
-getName1 = (callback) => {
-    setTimeout(()=> {
-        let name = "Hakan";
-        callback(name)
-    },1000)   
-}
-getSurname1 = (name) => {
-    setTimeout(()=> {
-        let surname = "Pekkaya";
-        console.log(name, surname);
-    },500)       
-}
-getName1(getSurname1)
-
-
-// 3rd alternative
-
-getName2 = (callback) => {
-    setTimeout(()=> {
-        let name = "Hakan";
-        callback(name)
-    },1000)   
-}
-getSurname2 = (name, surname) => {
-    setTimeout(()=> {
-        let surname = "Pekkaya";
-        callback(surname);
-    },500)       
-}
-getName2((name)=>{
-    getSurname2(name, (surname)=>{
-        console.log(name, surname)
+function getComments(url, id){
+    let newURL = prepareURL(url,id);
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", ()=>{
+        if(xhr.readyState===4 && xhr.status===200){
+            console.log(JSON.parse(xhr.responseText)) // converts JSON
+        }
     })
-})
+    xhr.open("GET", newURL);
+    xhr.send();
+}
+
+//  Brings all (id:null) values
+// getComments("https://jsonplaceholder.typicode.com/comments", null)
+
+//  Brings according to id (number)
+getComments("https://jsonplaceholder.typicode.com/comments", 2)
+
 
