8642f771a2c9acc584d53438c644b654d92efb35
Hpekkaya <Ahpek1970@yahoo.com>
2024-01-01 09:44:16 +0300

    fetch than catch

--- trial.js
+++ trial.js
@@ -1,1 +1,37 @@
 
+// -----  fetch API  -----
+
+// function getStudents(url){
+//     fetch(url)
+//     .then((result) => {
+//         return result.json()
+//     })
+//     .then((data)=>console.log(data) )
+//     .catch((err) =>console.log(err) )        
+// }
+
+// getStudents("students.json");
+
+// Geeting Data from "jsonplaceholder"
+
+function getData(url){
+    fetch(url)
+    .then((result) =>result.json()     )
+    .then((data)=>console.log(data) )
+    .catch((err) =>console.log(err) )        
+}
+// Getting Users
+getData("https://jsonplaceholder.typicode.com/users");
+
+// Getting Albums
+// getData("https://jsonplaceholder.typicode.com/albums");
+
+//  Second alternative
+function getData1(url){
+   return fetch(url)
+}
+// Getting Users
+getData1("https://jsonplaceholder.typicode.com/users")
+.then((result) =>result.json()  )
+.then((data)=>console.log(data) )
+.catch((err) =>console.log(err) )        
